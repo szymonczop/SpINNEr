9d9f2db3c13720753c968cdabcc6e809e0eea53e
szymonczop <szymon.czop@infermedica.com>
2021-01-19 21:12:56 +0100

    Implemented ProxG function from page 28 ready to check for test data.

---
+++ ProxG.py
@@ -0,0 +1,24 @@
+# Proximity operator for function G, where
+# G(C) := lambda_N*||C||_*
+# ||C||_* - nuclear norm of C
+import numpy as np
+
+
+def ProxG(D, W2, delta, lambda_N):
+    d_delta = D + W2 / delta
+    d_delta = (d_delta + d_delta.T) / 2  # czemu tutaj to robimy ? wiem że symetryczy ale czy coś nas to kosztuje ?
+    U, S, Vt = np.linalg.svd(d_delta)  # Ddelta = U*diag(S)*Vt
+    diagS = np.diag(S)
+    if np.allclose(d_delta, U @ diagS @ Vt) != True:
+        print("ProxG SVD not made properly")
+    Stsh = np.diag(np.sign(S) * np.maximum(np.abs(S) - lambda_N / delta, 0))  # soft tresholoding of singular values
+    c_new = U @ Stsh @ Vt
+    return c_new
+
+if __name__=="__main__":
+    D = np.random.randint(0, 10, size=(5, 5))
+    W2 = np.ones((5, 5)) - np.eye(5)
+    delta = 0.5
+    lambda_n = 2.5
+    c_new = ProxG(D, W2, delta, lambda_n)
+    print(c_new)
