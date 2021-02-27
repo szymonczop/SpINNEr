55c4f5803ec13a73700401e112dde0008601d9b2
szymonczop <szymon.czop@infermedica.com>
2021-02-27 11:07:01 +0100

    Min norm estim function done (with some changes)

---
+++ spinnerCode/min_norm_estim.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def calc_min_norm_estim(y, SVDAx):
+    # objects
+    p0 = SVDAx["U"].shape[0]
+    p = int((1+np.sqrt(1+8*p0))/2)
+
+    #SVD outputs
+    U      = SVDAx["U"]
+    Sdiag  = SVDAx["Sdiag"]
+    Vt     = SVDAx["Vt"]
+    idxs   = SVDAx["idxs"]
+
+    #Estimate
+    Vty  = Vt @ y * np.diag(Sdiag)
+    Bvec = U@Vty
+    Bestim  = np.zeros(p**2)
+    Bestim[idxs] = Bvec
+
+    Bestim  = Bestim.reshape(p,p) # p-by-p matrix
+    Bestim  = Bestim + Bestim.T
+
+    return Bestim
+
+calc_min_norm_estim(y, SVDAx)
+
+
