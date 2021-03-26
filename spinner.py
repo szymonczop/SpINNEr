a197ebd9a2c5803d04f6b51886267002a3bb18e7
szymonczop <szymon.czop@infermedica.com>
2021-03-26 22:50:56 +0100

    adapt spinner.py to cooperation with matlab

--- spinner.py
+++ spinner.py
@@ -3,7 +3,15 @@ import numpy as np
 # stworzenie macierzy  AA czyli wielu macierzy w jednej.
 n = 20
 p = 5
+y =np.array([4.7011,0.0157, 2.7057,-1.4226,5.5819,-3.7481, 10.0588,0.3313,-4.1411,5.1575,-6.2244, -3.1968,4.7178,
+             0.6485,-5.5586,-5.9589,-2.5582,2.5288,-0.8932,-2.6688])
 
+# macierz cech pacjetów
+np.random.seed(2021)
+X = np.random.randint(0, 20, size=(n,7))
+
+lambdaN = 1.2
+lambdaL = 0.9
 
 # data = AA.copy()
 # with open("best_file.txt", 'w') as outfile:
@@ -33,26 +41,39 @@ for matrix in AA:
         print("One of A_i has non zero value on diagonal")
 
 # checking if X was provided
-
 #To muszę dorobić
+
+#Checking: dimmension check
+if AA.shape[0] != len(y):
+    print('The third dimension of 3-way tensor containing Ai`s should be the same as the length of y')
+
+#Checking: dimmension check
+if X.shape[0] != len(y):
+    print('Number of rows in X and the length of y should coincide')
+
+#Objects
+nargin =2
+p = AA.shape[1]
+n = len(y)
+if nargin == 5: ## NIE WIEM CO TA ZMIENNA NIBY ROBI
+    if lambdaL > 0:
+        W = np.ones((p,p)) - np.eye(p,p)
+    else:
+        W   = np.ones((p,p))
+
+
+
 
 ##########
 ########## get rid of X from the optimization problem
 #########
-y =np.array([4.7011,0.0157, 2.7057,-1.4226,5.5819,-3.7481, 10.0588,0.3313,-4.1411,5.1575,-6.2244, -3.1968,4.7178,
-             0.6485,-5.5586,-5.9589,-2.5582,2.5288,-0.8932,-2.6688])
 
-y.shape
-
-
-np.random.seed(2021)
-X = np.random.randint(0, 20, size=(n,7)) # to jest macierz cech pacjętów
 
 H = np.eye(n) - X @ np.linalg.inv(X.T @ X) @ X.T
 AAmatrix = AA.reshape(n,-1)
-#AAmatrix.shape # 10x25  nxp^2
+#AAmatrix.shape # 20x25  nxp^2
 AAtilde = H @ AAmatrix
-#AAtilde.shape # 10,25
+#AAtilde.shape # 20,25
 AAtilde = AAtilde.reshape(n,p,p)
 #AAtilde.shape 10 x 5 x 5
 ytilde = H @ y
@@ -78,9 +99,8 @@ AvecsUP = 2 * Avecs[idxs,:]
 
 # Economy-size SVD
 U, Sdiag, Vt = np.linalg.svd(AvecsUP)  # U is (p^2-p)/2-by-n, S is n-by-n, V is n-by-n. # dostaje też inne wartośći niż założone
-print(f"U shape{U.shape}, S shape {S.shape}, Vt shape {Vt.shape}") # te wymiary nie zgadzają mi sie z założeniami
-
 S = np.diag(Sdiag)
+print(f"U shape{U.shape}, S shape {S.shape}, Vt shape {Vt.shape}") # te wymiary nie zgadzają mi sie z założeniami bbb
 np.allclose(AvecsUP, U[:,:Sdiag.shape[0]] @ S @ Vt[:Sdiag.shape[0],:])
 
 #########
@@ -94,13 +114,22 @@ SVDAx["Vt"] =Vt[:middle_product.shape[0],:]
 SVDAx["idxs"]  = idxs
 
 ## Cases
-lambdaN = 1.2
-lambdaL = 0.9
-
 solverType = [x>0 for x in [lambdaN, lambdaL]]
 solverType = solverType[0] + 2*solverType[1] + 1
 
 
+## Solver
+
+if solverType == 1:
+    pass
+elif solverType == 2:
+    pass
+elif solverType == 3:
+    pass
+elif solverType == 4:
+    pass
+
+## Estimates
 
 
 
